e5342085e9f8455959c2d893958c3f4cc241a2cf
decklord <camilo.lopez.a@gmail.com>
2013-09-27 14:50:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var words, timer, speed, index;
+
+$("#stop-reading").hide();
+
+function stop(){
+    intro = "Type something bellow and I will make you read it faster.";
+    $("#word-container center").html(intro);
+    clearInterval(timer);
+    toggle();
+}
+
+function toggle(){
+    $("#start-reading").toggle(200);
+    $("#stop-reading").toggle(200);
+    $("#text-to-read").toggle(200);
+}
+
+$("#start-reading").click(function(){
+    toggle();
+    wpm = $("#wpm").val();
+    speed = 60*1000/wpm;
+    index = 0;
+    words = $("#text-to-read").val().replace(/,/g," ").replace(/\./g," ").split(" ");
+    timer = setInterval(function(){
+        if (index >= words.length) {
+            stop();
+        } else {
+            word = words[index];
+            if (word === "" || word === " ") word = "&nbsp;";
+            $("#word-container center").html(word);
+            index++;
+        }
+    }, speed);
+
+});
+
+$("#stop-reading").click(function(){
+    stop();
+});
